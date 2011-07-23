8ee46b15a8ccaa3fe9735c5a00fd0ca417a77786
Lars Marius Garshol <larsga@gmail.com>
2011-07-23 07:58:00 -0400

    Merge.

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ Configuration settings for deployment.
 """
 
 # where session data is stored on disk
-SESSION_DIR = 'sessions'
+SESSION_DIR = '/home/webadmin/whazzup.garshol.priv.no/sessions'
 
 # where the word databases (DBMs) are stored
-DBM_DIR = 'dbms/'
+DBM_DIR = '/home/webadmin/whazzup.garshol.priv.no/dbms/'
 
 # database connection string
-DB_CONNECT_STRING = 'dbname=whazzup'
+DB_CONNECT_STRING = 'dbname=whazzup user=larsga password=u54raud host=localhost'
