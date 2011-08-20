8e0710fcc6d446013942ba533bf42fc91f98086d
Lars Marius Garshol <larsga@gmail.com>
2011-08-20 13:16:43 +0200

    Moved feed downloading out to a separate thread pool, thus greatly improving throughput. Should scale to more than 3000 users now before we need to change anything.

--- config.py
+++ config.py
@@ -12,10 +12,19 @@ DBM_DIR = 'dbms/'
 DB_CONNECT_STRING = 'dbname=whazzup'
 
 # key number for sysv_ipc message queue
-QUEUE_NUMBER = 6331
+QUEUE_NUMBER = 6332
 
 # where to put queue statistics reports
 STATS_DIR = '.'
 
 # where to put the vector cache (marshal files)
 VECTOR_CACHE_DIR = '/tmp'
+
+# the maximum number of users we allow
+MAX_USERS = 12
+
+# number of feed downloading threads
+DOWNLOAD_THREADS = 5
+
+# temporary storage for downloaded feeds
+FEED_CACHE = '/tmp'
