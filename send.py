c18da98be74ea85681fe741ae52afbd663a4cf1a
Lars Marius Garshol <larsga@gmail.com>
2012-07-07 05:04:22 -0400

    Bug fix.

--- send.py
+++ send.py
@@ -1,7 +1,8 @@
 
+from config import QUEUE_FILE
 import sys
 import sysv_ipc
 
-no = int(open("queue-no.txt").read())
+no = int(open(QUEUE_FILE).read())
 mqueue = sysv_ipc.MessageQueue(no)
 mqueue.send(sys.argv[1])
